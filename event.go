b76e99f36b48eb03e04d5dfb2c4886deb4cd3ae7
Daniil <d.stepanenko@noisyminer.com>
2020-04-04 22:41:48 +0300

    remove useless tag

--- event.go
+++ event.go
@@ -5,10 +5,10 @@ import "time"
 // Event is the JSON struct that `go test -json` emit.
 // More info: https://golang.org/cmd/test2json/#hdr-Output_Format
 type Event struct {
-	Time    time.Time `json:",omitempty"`
-	Action  Action    ``
-	Package string    `json:",omitempty"`
-	Test    string    `json:",omitempty"`
-	Elapsed duration  `json:",omitempty"`
-	Output  string    `json:",omitempty"`
+	Time    time.Time
+	Action  Action
+	Package string   `json:",omitempty"`
+	Test    string   `json:",omitempty"`
+	Elapsed duration `json:",omitempty"`
+	Output  string   `json:",omitempty"`
 }
